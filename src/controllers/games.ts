f48b0f76cf13a185e06afe17bb6f0a77fe0951e6
siddharth <ranasiddharth6@gmail.com>
2022-12-21 21:40:28 +0530

    list games api

---
+++ src/controllers/games.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import axios from "axios";
+
+export const list = async (req: Request, res: Response) => {
+  const { page = 1, search = "" } = req.query;
+  try {
+    const gamesRes = await axios.get(
+      `https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&page=${page}&search=${search}`,
+      {
+        headers: {
+          "Accept-Encoding": "*",
+        },
+      }
+    );
+    res.json({
+      count: gamesRes.data.count,
+      games: gamesRes.data.results,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "An error occoured please try again later",
+    });
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+import gamesRouter from "./routes/games";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -9,5 +9,7 @@ const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.send("ok");
 });
+
+app.use("/api/games", gamesRouter);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
