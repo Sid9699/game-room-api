cd1e2423fadbeb19ba0caf811df0f03281f53a07
siddharth <ranasiddharth6@gmail.com>
2023-04-19 22:59:08 +0530

    adding games api in swagger

--- src/routes/games.route.ts
+++ src/routes/games.route.ts
@@ -2,9 +2,54 @@ import express from "express";
 import { list, listGenres } from "../controllers/games.controller";
 import passport from "passport";
 
+/**
+ * @swagger
+ * tags:
+ *   name: Games
+ */
 const router = express.Router();
 
+/**
+ * @swagger
+ * /games:
+ *   get:
+ *     tags: [Games]
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *       - in: query
+ *         name: pageSize
+ *         schema:
+ *           type: integer
+ *           default: 10
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: genres
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description:
+ *
+ */
 router.get("/", passport.authenticate("jwt", { session: false }), list);
+
+/**
+ * @swagger
+ * /games/genres:
+ *   get:
+ *     tags: [Games]
+ *     responses:
+ *       200:
+ *         description:
+ *
+ */
 router.get(
   "/genres",
   passport.authenticate("jwt", { session: false }),
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,6 +4,16 @@ import { get } from "../controllers/user.controller";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     tags: [Auth]
+ *     responses:
+ *       200:
+ *         description:
+ *
+ */
 router.get("/", passport.authenticate("jwt", { session: false }), get);
 
 export default router;
